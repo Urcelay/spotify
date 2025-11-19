4d838340f7f524398f2ec16b4521291fcca1d17b
Urcelay <fernandaurcelay5@gmail.com>
2025-11-18 22:52:06 -0600

    first day

---
+++ database/migrations/2025_11_19_041443_create_albums_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_artists')->constrained('artists')->onDelete('cascade');
+            $table->string('name');
+            $table->string('cover_image')->nulltable();
+            $table->integer('release_year')->nullable();
+            $table->integer('total_songs')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('albums');
+    }
+};
---
+++ database/migrations/2025_11_19_041453_create_music_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('music', function (Blueprint $table) {
+            $table->string('title');
+            $table->foreignId('id_category')->constrained('categories')->onDelete('cascade');
+            $table->foreignId('id_artist')->constrained('artists')->onDelete('cascade');
+            $table->foreignId('id_album')->nullable()->constrained('albums')->onDelete('set null');
+            $table->integer('duration')->nullable();
+            $table->bigInteger('size')->nullable(); // en bytes
+            $table->string('file_name');
+            $table->string('file_url');
+            $table->string('cover_image')->nullable();
+            $table->integer('play_count')->default(0);
+            $table->bigInteger('likes')->default(0);
+            $table->boolean('is_featured')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('music');
+    }
+};
